3b021ba8144c1cf1476ad2ac318ec5cb962f84d9
jiamid <jiamid.t@binarywalk.com>
2024-07-30 14:19:58 +0800

    更新start回复

--- tg_bot/handlers/messages.py
+++ tg_bot/handlers/messages.py
@@ -12,6 +12,8 @@ from aiogram.filters import CommandStart, Command
 from aiogram.utils.markdown import hbold
 from aiogram.types import Message
 from tg_bot.bot import telegram_router
+from commonts.util import to_escape_string
+from commonts.storage_manager import proxy_manager
 
 
 @telegram_router.message(Command("id"))
@@ -21,7 +23,18 @@ async def cmd_id(message: Message) -> None:
 
 @telegram_router.message(CommandStart())
 async def cmd_start(message: Message) -> None:
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!",parse_mode='html')
+    await message.answer(f'*Hello {to_escape_string(message.from_user.first_name)}*\n'
+                         f'_Region:_\n'
+                         f'{proxy_manager.get_keys_str()}\n'
+                         f'\n`/list_keywords`'
+                         f'\n`/add_keyword 关键字`'
+                         f'\n`/del_keyword 关键字`'
+                         f'\n`/join`'
+                         f'\n`/exit`'
+                         f'\n`/list_proxys`'
+                         f'\n`/set_proxy 代理区域 代理链接`'
+                         f'\n`/del_proxy 代理区域`'
+                         f'\n\n', parse_mode='MarkdownV2')
 
 
 @telegram_router.message(F.text == "echo")
