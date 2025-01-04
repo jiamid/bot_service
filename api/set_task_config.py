bf5e74f913aafadad85874b281946bf30ab9f677
jiamid <jiamid@qq.com>
2025-01-04 19:45:30 +0800

    update set task

--- api/set_task_config.py
+++ api/set_task_config.py
@@ -21,9 +21,9 @@ class TaskConfigRespModel(BaseResponseModel):
 
 
 @router.post("/set_task_config", response_model=TaskConfigRespModel)
-async def set_task_config(new_task_config: TaskConfigModel):
-    timer_task_storage.set_value("keywords", new_task_config.keywords, False)
-    timer_task_storage.set_value("targets", new_task_config.targets)
+async def set_task_config(keywords: list[str], targets: list[str]):
+    timer_task_storage.set_value("keywords", keywords, False)
+    timer_task_storage.set_value("targets", targets)
     keywords = timer_task_storage.get_value("keywords", [])
     targets = timer_task_storage.get_value("targets", [])
     return TaskConfigRespModel(data=TaskConfigModel(keywords=keywords, targets=targets))
