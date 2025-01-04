252f5da99679a0885a59ce944ae56bbce245c1ce
jiamid <jiamid@qq.com>
2025-01-04 20:09:36 +0800

    add html

--- api/set_task_config.py
+++ api/set_task_config.py
@@ -3,10 +3,12 @@
 # @FileName :set_task_config.py
 # @Time :2024/01/04 19:39
 # @Author :Jiamid
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from commonts.storage_manager import timer_task_storage
 from commonts.base_model import BaseResponseModel
 from pydantic import BaseModel, Field
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
@@ -27,3 +29,11 @@ async def set_task_config(keywords: list[str], targets: list[str]):
     keywords = timer_task_storage.get_value("keywords", [])
     targets = timer_task_storage.get_value("targets", [])
     return TaskConfigRespModel(data=TaskConfigModel(keywords=keywords, targets=targets))
+
+
+templates = Jinja2Templates(directory='templates')
+
+
+@router.get("/set_task_config", response_class=HTMLResponse)
+async def set_task_config_html(request: Request):
+    return templates.TemplateResponse('set_task_config_page.html', {'request': request})
